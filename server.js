28c0cf361fe6eea423b38b3732326e8903af7437
Ori <oorishema@gmail.com>
2021-01-06 15:19:06 +0200

    version 1.0.0

--- server.js
+++ server.js
@@ -61,9 +61,9 @@ app.use(errorHandler);
 //serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static('./winner-front/build'))
+    app.use(express.static('client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'winner-front', "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, 'client', "build", "index.html"));
     })
 }
 
